bd34d9ead5dee904788aa730b4b39e147f5a3fff
Jonathon Béasse <crypticspaghetti@protonmail.com>
2022-05-27 13:48:13 -0600

    Add winston to replace console log for server logging

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const winston = require('winston')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+const logger = winston.createLogger({
+  level: process.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    process.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+    })
+  ]
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  logger.info('server started on port 5000')
 })
